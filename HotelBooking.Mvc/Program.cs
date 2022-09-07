63ecdc3526053215743972b9abc626a5c7bebf05
kasp608b <54952896+kasp608b@users.noreply.github.com>
2022-09-07 18:59:57 +0200

    Updated to .Net 6

--- HotelBooking.Mvc/Program.cs
+++ HotelBooking.Mvc/Program.cs
@@ -1,17 +1,63 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using HotelBooking.Core;
+using HotelBooking.Infrastructure;
+using HotelBooking.Infrastructure.Repositories;
+using HotelBooking.Mvc.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace HotelBooking.Mvc
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HotelBookingContext>(opt => opt.UseInMemoryDatabase("HotelBookingDb"));
+
+builder.Services.AddScoped<IRepository<Room>, RoomRepository>();
+builder.Services.AddScoped<IRepository<Customer>, CustomerRepository>();
+builder.Services.AddScoped<IRepository<Booking>, BookingRepository>();
+builder.Services.AddScoped<IBookingManager, BookingManager>();
+builder.Services.AddScoped<IBookingViewModel, BookingViewModel>();
+builder.Services.AddTransient<IDbInitializer, DbInitializer>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+    app.UseDeveloperExceptionPage();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+    // Initialize the database.
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var dbContext = services.GetService<HotelBookingContext>();
+        var dbInitializer = services.GetService<IDbInitializer>();
+        dbInitializer.Initialize(dbContext);
     }
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
--- HotelBooking.WebApi/Program.cs
+++ HotelBooking.WebApi/Program.cs
@@ -1,17 +1,47 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using HotelBooking.Core;
+using HotelBooking.Infrastructure;
+using HotelBooking.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
-namespace HotelBooking.WebApi
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<HotelBookingContext>(opt => opt.UseInMemoryDatabase("HotelBookingDb"));
+
+builder.Services.AddScoped<IRepository<Room>, RoomRepository>();
+builder.Services.AddScoped<IRepository<Customer>, CustomerRepository>();
+builder.Services.AddScoped<IRepository<Booking>, BookingRepository>();
+builder.Services.AddScoped<IBookingManager, BookingManager>();
+builder.Services.AddTransient<IDbInitializer, DbInitializer>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+    // Initialize the database.
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var dbContext = services.GetService<HotelBookingContext>();
+        var dbInitializer = services.GetService<IDbInitializer>();
+        dbInitializer.Initialize(dbContext);
     }
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
