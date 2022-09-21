eb3e29adad987fa42d2de11e8370f48a682415f5
scupak <antonibujko@hotmail.com>
2022-09-21 19:14:15 +0200

    Update BookingManagerTests.cs

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -134,30 +134,6 @@ namespace HotelBooking.UnitTests
             Assert.False(result);
             fakeBookingRepository.Verify(x => x.Add(It.IsAny<Booking>()), Times.Never);
         }
-        /*
-         public List<DateTime> GetFullyOccupiedDates(DateTime startDate, DateTime endDate)
-        {
-            if (startDate > endDate)
-                throw new ArgumentException("The start date cannot be later than the end date.");
-
-            List<DateTime> fullyOccupiedDates = new List<DateTime>();
-            int noOfRooms = roomRepository.GetAll().Count();
-            var bookings = bookingRepository.GetAll();
-
-            if (bookings.Any())
-            {
-                for (DateTime d = startDate; d <= endDate; d = d.AddDays(1))
-                {
-                    var noOfBookings = from b in bookings
-                                       where b.IsActive && d >= b.StartDate && d <= b.EndDate
-                                       select b;
-                    if (noOfBookings.Count() >= noOfRooms)
-                        fullyOccupiedDates.Add(d);
-                }
-            }
-            return fullyOccupiedDates;
-        }
-         */
 
         [Theory]
         [MemberData(nameof(GetData), parameters: TestData.DateAndExpectedDate)]
