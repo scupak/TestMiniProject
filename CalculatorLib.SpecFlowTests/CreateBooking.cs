dac465e175c10751046e7d938021efa4ad1b9d14
kasp608b <54952896+kasp608b@users.noreply.github.com>
2022-10-07 11:35:09 +0200

    hmmm

---
+++ CalculatorLib.SpecFlowTests/CreateBooking.cs
@@ -0,0 +1,36 @@
+﻿using TechTalk.SpecFlow;
+using Xunit;
+
+namespace HotelBooking.Spec
+{
+    [Binding]
+    public class CreateBooking
+    {
+        int firstNumber, secondNumber, sum;
+
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int a)
+        {
+            firstNumber = a;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int b)
+        {
+            secondNumber = b;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedSum)
+        {
+            Assert.Equal(expectedSum, sum);
+        }
+    }
+}
---
+++ CalculatorLib.SpecFlowTests/CreateBooking.feature
@@ -0,0 +1,15 @@
+﻿Feature: CreateBooking
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario Outline: Add two numbers
+	Given I have entered <firstNumber> into the calculator
+	And I have also entered <secondNumber> into the calculator
+	When I press add
+
+	Examples:
+	| firstNumber | secondNumber | sum |
+	| 50          | 70           | 120 |
+	| 30          | 20           |  50 |
